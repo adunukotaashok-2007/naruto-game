6603c60dfe8099d1564ac2a6dfb1c3a6fe246310
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-08-26 21:52:49 +0530

    Add character data for Naruto universe

---
+++ src/entities/player/characters.js
@@ -0,0 +1,74 @@
+const CHARACTERS = [
+
+    {
+        id: "naruto",
+        name: "NARUTO",
+        icon: "🥷",
+        color: "#ff9800",
+        health: 100,
+        chakra: 100,
+        attack: 10,
+        jutsu: 25,
+        speed: 5
+    },
+
+    {
+        id: "sasuke",
+        name: "SASUKE",
+        icon: "⚔️",
+        color: "#7c4dff",
+        health: 100,
+        chakra: 100,
+        attack: 12,
+        jutsu: 30,
+        speed: 5
+    },
+
+    {
+        id: "kakashi",
+        name: "KAKASHI",
+        icon: "🥷",
+        color: "#42a5f5",
+        health: 110,
+        chakra: 100,
+        attack: 11,
+        jutsu: 28,
+        speed: 4
+    },
+
+    {
+        id: "sakura",
+        name: "SAKURA",
+        icon: "🌸",
+        color: "#ff4081",
+        health: 120,
+        chakra: 90,
+        attack: 14,
+        jutsu: 24,
+        speed: 4
+    },
+
+    {
+        id: "shikamaru",
+        name: "SHIKAMARU",
+        icon: "🌑",
+        color: "#78909c",
+        health: 100,
+        chakra: 110,
+        attack: 9,
+        jutsu: 32,
+        speed: 4
+    },
+
+    {
+        id: "gaara",
+        name: "GAARA",
+        icon: "🏜️",
+        color: "#c62828",
+        health: 130,
+        chakra: 90,
+        attack: 10,
+        jutsu: 27,
+        speed: 3
+    }
+];
